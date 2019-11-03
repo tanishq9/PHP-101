6298431070f1a3cedbe0b5affee8099ebd26be69
Tanishq <tanishqsaluja18@gmail.com>
2019-11-04 01:09:07 +0530

    added basic php projects

---
+++ ASCII First Letter/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Tanishq Saluja</title>
+	</head>
+	<body>
+		<h1>Tanishq Saluja PHP</h1>
+		<pre>ASCII ART (First letter of the Name 'Tanishq' - 'T'):
+
+    *****
+      *
+      *
+      *
+      *
+  	</pre>
+
+		<p>
+			The SHA256 hash of "Tanishq Saluja" is <?php print hash('sha256', 'Tanishq Saluja'); ?>
+		</p>
+		<a href="fail.php">Click here to cause a traceback</a>
+		<br>
+		<a href="check.php">Click here to check the error setting</a>
+		<br>
+	</body>
+</html>
